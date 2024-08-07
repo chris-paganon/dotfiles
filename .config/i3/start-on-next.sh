eea6689169ebb676f00c1d0976f4bae2c6445aa5
chrispaganon <info@chrispaganon.com>
2024-08-06 20:02:03 -0400

    Start & move apps directly to new workspace

---
+++ .config/i3/start-on-next.sh
@@ -0,0 +1,6 @@
+# requires jq
+
+# Get the current workspace
+NEXT_AVAILABLE_WORKSPACE=$(echo $(i3-msg -t get_workspaces | jq '.[] | .num' | sort -n) | awk '{for(i=1;i<=NF+1;i++)if($i!=i){print i;exit}}')
+
+i3-msg workspace $NEXT_AVAILABLE_WORKSPACE; i3-msg exec $1
