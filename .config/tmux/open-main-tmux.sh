b749c821c43be858ed61f7588a01b2456abed7a2
chrispaganon <info@chrispaganon.com>
2024-11-23 13:44:37 +0100

    open tmux as default for zsh

--- .config/tmux/open-main-tmux.sh
+++ .config/tmux/open-main-tmux.sh
@@ -1,10 +1,17 @@
+#!/bin/bash
+
+# If tmux is not installed, not running on X11 or already inside TMUX, exit
+if [ ! -x "$(command -v tmux)" ] || [ -z "${DISPLAY}" ] || [ -n "${TMUX}" ]; then
+  return
+fi
+
 current_dir=$(pwd)
 tmux has-session -t main
 
-# If the session does not exist, create it
+# If no session exists, create one and attach to it
 if [ $? != 0 ]; then
-  tmux new -A -s main -c $current_dir
-  exit 0
+  exec tmux new -A -s main -c $current_dir
+  return
 fi
 
 attached_session=$(tmux list-sessions | grep -c "main.*(attached)")
@@ -13,12 +20,14 @@ if [ $attached_session -eq 1 ]; then
   if [ $current_dir != $HOME ]; then
     tmux attach -t main \; new-window -c $current_dir \; detach
   fi
-  # Run i3-msg to focus on the xfce4-terminal
-  i3-msg '[class="Xfce4-terminal"] focus; mark "terminal"'
+  # Run i3-msg to focus on the xfce4-terminal and exit the current shell/terminal
+  i3-msg '[class="Xfce4-terminal"] focus; mark "terminal"'a
+  exit 0
 else
+  # If we are not attached to the session already, attach to it and maybe create a new window in the current directory
   if [ $current_dir = $HOME ]; then
-    tmux attach -t main
+    exec tmux attach -t main
   else
-    tmux attach -t main \; new-window -c $current_dir
+    exec tmux attach -t main \; new-window -c $current_dir
   fi
 fi
--- .zshrc
+++ .zshrc
@@ -182,3 +182,5 @@ eval "$(zoxide init zsh)"
 export NVM_DIR="$HOME/.config/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+source ~/.config/tmux/open-main-tmux.sh
