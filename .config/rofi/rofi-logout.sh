e33c7793821ba72ddae5c7d1c51c2c9788ae97cb
chrispaganon <info@chrispaganon.com>
2024-11-25 10:40:03 +0100

    add custom rofi logout

---
+++ .config/rofi/rofi-logout.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ -n "$1" ]]; then
+  if [[ "$1" == "logout" ]]; then
+    xfce4-session-logout --logout --fast
+  elif [[ "$1" == "suspend" ]]; then
+    xfce4-session-logout --suspend --fast
+  elif [[ "$1" == "reboot" ]]; then
+    xfce4-session-logout --reboot --fast
+  elif [[ "$1" == "shutdown" ]]; then
+    shutdown now
+  fi
+  exit
+fi
+
+echo "shutdown"
+echo "suspend"
+echo "reboot"
+echo "logout"
+echo "cancel"
