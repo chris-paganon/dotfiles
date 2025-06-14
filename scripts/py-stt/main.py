cac8948aee1f43d79cd5c1f80232d64369169241
chrispaganon <info@chrispaganon.com>
2025-06-14 15:15:48 +0200

    add basic whisper transcription

--- scripts/py-stt/main.py
+++ scripts/py-stt/main.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+import whisper
+import pyperclip
+
+model = whisper.load_model("turbo")
+
+print("Transcribing...")
+result = model.transcribe("./outputs/test1.wav")
+
+print(result["text"])
+pyperclip.copy(result["text"])
