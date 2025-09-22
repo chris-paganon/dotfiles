355da6e8a656103611ae2bda74f09e3a5d3b3d23
chrispaganon <info@chrispaganon.com>
2025-09-22 12:39:09 +0200

    plutaro full stack also runs the commands

--- .config/tmux/plutaro-full-stack.sh
+++ .config/tmux/plutaro-full-stack.sh
@@ -10,9 +10,14 @@ if [ -z "$TMUX" ]; then
 fi
 
 # Split current window horizontally to create bottom pane (./frontend)
-tmux split-window -v -p 20 -c "$current_dir/frontend"
+# run the command VITE_USE_LOCAL_FUNCTIONS=true pnpm run dev in the pane
+tmux split-window -v -p 20 -c "$current_dir/frontend" "VITE_USE_LOCAL_FUNCTIONS=true pnpm run dev"
 
 tmux select-pane -t 0
 
 # Split the bottom pane vertically to create ./functions/typescript pane  
-tmux split-window -v -p 28 -c "$current_dir/functions/typescript"
+tmux split-window -v -p 28 -c "$current_dir/functions/typescript" "pnpm run build:watch"
+
+tmux select-pane -t 0
+
+firebase emulators:start --only functions
